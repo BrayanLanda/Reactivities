bc9ea22a9843ea716c3d83e88fc666eea58db058
Brayan Acosta <landakkpp@gmail.com>
2024-09-19 22:20:06 -0500

    using mobx

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,14 @@
+import { makeObservable, observable } from "mobx";
+
+export default class ActivityStore {
+    title = 'Hello from Mobx';
+
+    /**
+     *
+     */
+    constructor() {
+        makeObservable(this, {
+            title: observable
+        })
+    }
+}
