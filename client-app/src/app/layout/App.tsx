a99870714f8ff84c3d85c51c53923fb3a4e904fb
Brayan Acosta <landakkpp@gmail.com>
2024-09-23 23:10:40 -0500

    add navigation in navbar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,27 +1,19 @@
-import { useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
-import LoadingComponent from "./LoadingComponent";
-import { useStore } from "../stores/store";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { observer } from 'mobx-react-lite';
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const {activityStore} = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if(activityStore.loadingInitial) return <LoadingComponent content="Loading App" />
+  
 
   return (
     <div>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard />
+        <Outlet />
       </Container>
     </div>
   );
 }
 
-export default App;
+export default observer(App);
