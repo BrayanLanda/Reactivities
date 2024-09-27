f2372d4792d9e2c3cf22c8c2b4a9a145251f688d
Brayan Acosta <landakkpp@gmail.com>
2024-09-26 19:38:10 -0500

    updating router - moving the home page outside of nav

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,18 +1,25 @@
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
-import { observer } from 'mobx-react-lite';
-import { Outlet } from "react-router-dom";
+import { observer } from "mobx-react-lite";
+import { Outlet, useLocation } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
 
 function App() {
-  
+  const location = useLocation();
 
   return (
-    <div>
-      <NavBar />
-      <Container style={{ marginTop: "7em" }}>
-        <Outlet />
-      </Container>
-    </div>
+    <>
+      {location.pathname === "/" ? (
+        <HomePage />
+      ) : (
+        <>
+          <NavBar />
+          <Container style={{ marginTop: "7em" }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
+    </>
   );
 }
 
