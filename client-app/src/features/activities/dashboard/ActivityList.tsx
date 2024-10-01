400f45ca3905c97f9fef024f564421dfd8bf3ccc
Brayan Acosta <landakkpp@gmail.com>
2024-09-30 22:02:33 -0500

    add styles to cards of actvities

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,21 +1,25 @@
-import { Item, Segment } from "semantic-ui-react";
+import { Header } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import ActivityListItem from "./ActivityListItem";
+import { Fragment } from "react/jsx-runtime";
 
+export default observer(function ActivityList() {
+  const { activityStore } = useStore();
+  const { groupeActivities } = activityStore;
 
-export default observer(function ActivityList(){
-
-    const {activityStore} = useStore();
-    const {activitiesByDate} = activityStore;
-   
-    return(
-        <Segment>
-            <Item.Group divided>
-                {activitiesByDate.map(activity => (
-                    <ActivityListItem key={activity.id} activity={activity} />
-                ))}
-            </Item.Group>
-        </Segment>
-    );
+  return (
+    <>
+      {groupeActivities.map(([group, activities]) => (
+        <Fragment>
+          <Header sub color="teal">
+            {group}
+          </Header>
+          {activities.map((activity) => (
+            <ActivityListItem key={activity.id} activity={activity} />
+          ))}
+        </Fragment>
+      ))}
+    </>
+  );
 });
