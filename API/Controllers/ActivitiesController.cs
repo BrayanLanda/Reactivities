f12c42984fd327c3076772f790f8ead6246aa5c1
Brayan Acosta <landakkpp@gmail.com>
2024-12-06 22:49:42 -0500

    add endpoint in activity for update attend and add updateda attend class in activities application

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -43,7 +43,12 @@ namespace API.Controllers
         public async Task<IActionResult> Delete(Guid id)
         {
             return HandleResult(await Mediator.Send(new Delete.Command { Id = id}));
+        }
 
+         [HttpPost("{id}/attend")]
+        public async Task<IActionResult> Attend(Guid id)
+        {
+            return HandleResult(await Mediator.Send(new UpdateAttendance.Command { Id = id }));
         }
     }
 }
